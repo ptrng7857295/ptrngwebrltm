14dcd670d67babe0284451570355b07620947fd2
ptrng7857295 <donnyso@protonmail.com>
2026-08-29 07:09:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ EXCHANGE_API_KEY = os.getenv("EXCHANGE_API_KEY", "")
 EXCHANGE_API_URL = f"https://v6.exchangerate-api.com/v6/{EXCHANGE_API_KEY}/pair/USD/IDR"
 
 # ─── KONSTANTA ANTAM ────────────────────────────────────────
-ANTAM_JUAL_MARKUP    = 1.114    # +11.4% dari harga spot
-ANTAM_BUYBACK_MARKUP = 1.009   # -7.5% dari harga spot
+ANTAM_JUAL_MARKUP   = 1.038   # 1.112 (+11.2%) dari harga spot (sesuaikan dengan kondisi pasar) menjadi 1.038
+ANTAM_BUYBACK_MARKUP = 0.989 # 1.009 (+0.9%) dari harga spot menjadi 0.989
